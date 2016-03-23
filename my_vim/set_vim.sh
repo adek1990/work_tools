65060fb1e2a2e5832847f49c6cde5271845d95f2
eager7 <panchangtao@gmail.com>
2016-03-22 20:45:57 -0700

    my vim

---
+++ my_vim/set_vim.sh
@@ -0,0 +1,8 @@
+sudo apt-get install vim vim-scripts vim-doc ctags vim-addon-manager
+tar -xvf vimcdoc-1.9.0.tar.gz 
+cd vimcdoc-1.9.0 && ./vimcdoc.sh -i
+cd -
+rm vimcdoc-1.9.0 -rf
+vim-addons install omnicppcomplete
+vim-addons install taglist
+
