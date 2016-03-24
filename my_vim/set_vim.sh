cfbdb1d9bf9c3ba387800c5a67514f1b3c1dc3a4
eager7 <panchangtao@gmail.com>
2016-03-23 23:27:10 -0700

    shadow

--- my_vim/set_vim.sh
+++ my_vim/set_vim.sh
@@ -1,8 +1,12 @@
 sudo apt-get install vim vim-scripts vim-doc ctags vim-addon-manager
-tar -xvf vimcdoc-1.9.0.tar.gz 
+tar -xvf vimcdoc-1.9.0.tar.gz  
 cd vimcdoc-1.9.0 && ./vimcdoc.sh -i
 cd -
 rm vimcdoc-1.9.0 -rf
 vim-addons install omnicppcomplete
 vim-addons install taglist
+echo "alias ctags='ctags -R --c++-kinds=+p --fields=+iaS --extra=+q'" >> ~/.bashrc
+
+
+
 
