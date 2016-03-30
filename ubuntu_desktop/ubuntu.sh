e552638e03cf82e824e82390a31b4de60539c525
pct <pct@ubuntu-15.10>
2016-03-30 18:40:12 +0800

    unity teawk

---
+++ ubuntu_desktop/ubuntu.sh
@@ -0,0 +1,11 @@
+sudo add-apt-repository ppa:peterlevi/ppa
+sudo apt-get update
+sudo add-apt-repository ppa:atareao/atareao
+sudo apt-get update
+sudo apt-get install indicator-multiload 
+sudo apt-get install unity-tweak-tool
+sudo apt-get install indicator-cpufreq 
+sudo apt-get install my-weather-indicator
+sudo apt-get install variety 
+sudo apt-get install diodon 
+sudo apt-get install diodon-plugins 
