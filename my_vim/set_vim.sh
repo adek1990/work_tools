a35d47ef636640e1566c09856e5bb10095976401
eager7 <panchangtao@gmail.com>
2016-04-01 10:28:05 +0800

    vim

--- my_vim/set_vim.sh
+++ my_vim/set_vim.sh
@@ -1,3 +1,7 @@
+tar -jxvf Vundle.vim.tar.bz2
+
+mkdir -p ~/.vim/bundle
+cp -rf ./Vundle.vim ~/.vim/bundle/
 sudo apt-get install vim vim-scripts vim-doc ctags vim-addon-manager
 tar -xvf vimcdoc-1.9.0.tar.gz  
 cd vimcdoc-1.9.0 && ./vimcdoc.sh -i
