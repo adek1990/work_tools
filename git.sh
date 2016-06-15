1c88f05327cd1a1fa6b0c1faf94a1c267fd5ac48
eager7 <panchagntao@gmail.com>
2016-06-15 17:59:59 +0800

    git

---
+++ git.sh
@@ -0,0 +1,6 @@
+cd ~
+echo "https://eager7:pct1197639@github.com" > .git-credentials
+git config --global credential.helper store
+git config --global core.editor vim
+git config --global user.email panchagntao@gmail.com
+git config --global user.name eager7
