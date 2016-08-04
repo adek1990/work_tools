f149360904ccb9a4afd6611a76f91faa434fe5c5
eager7 <panchagntao@gmail.com>
2016-08-03 19:16:42 -0700

    add file

--- shell.sh
+++ shell.sh
@@ -1,3 +1,5 @@
 sudo mount -t cifs -o file_mode=0777,dir_mode=0777,username=changtao.pan,password=pct1197639 //192.168.4.103/Kuang-Chi /home/pct/Windows/
 
 sudo mount.cifs -o username=firefly,password=firefly //192.168.4.10/firefly firefly/
+
+alias rys='rsync -av --exclude .git'
