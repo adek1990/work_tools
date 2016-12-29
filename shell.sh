4c7f01a1882132bf99c531fe908d35eaba6229cc
eager7 <panchagntao@gmail.com>
2016-12-29 11:09:54 +0800

    mount ntfs

--- shell.sh
+++ shell.sh
@@ -2,5 +2,7 @@ sudo mount -t cifs -o file_mode=0777,dir_mode=0777,username=changtao.pan,passwor
 
 sudo mount.cifs -o username=firefly,password=firefly //192.168.4.10/firefly firefly/
 
+UUID=8592F969687A7E01	 		  /home/pct/workspace ntfs  defaults,utf8,uid=1000,gid=1000,dmask=022,fmask=033  0   0
+
 alias rys='rsync -av --exclude .git'
 export PATH=$PATH:~/.local/bin
