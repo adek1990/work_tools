941f3a5d895401b228061ee0c9a256fae08c2230
eager7 <panchangtao@gmail.com>
2019-01-04 10:57:57 +0800

    igt

--- git.sh
+++ git.sh
@@ -2,5 +2,5 @@ cd ~
 echo "https://eager7:pct1197639@github.com" > .git-credentials
 git config --global credential.helper store
 git config --global core.editor vim
-git config --global user.email panchagntao@gmail.com
+git config --global user.email panchangtao@gmail.com
 git config --global user.name eager7
