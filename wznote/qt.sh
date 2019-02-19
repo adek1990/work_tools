803d06a9fb54af5ca6aa8c2f1eefabd7156753fc
eager7 <panchangtao@gmail.com>
2019-02-19 19:32:14 +0800

    qt

---
+++ wznote/qt.sh
@@ -0,0 +1,6 @@
+$ sudo apt-get install fcitx-frontend-qt5
+plainchant@plainchant-pct:~/workspace/work_tools$ sudo cp /usr/lib/x86_64-linux-gnu/qt5/plugins/platforminputcontexts/libfcitxplatforminputcontextplugin.so /opt/Qt5.9.0/Tools/QtCreator/lib/Qt/plugins/platforminputcontexts/
+[sudo] password for plainchant:
+plainchant@plainchant-pct:~/workspace/work_tools$ sudo chmod +x /opt/Qt5.9.0/Tools/QtCreator/lib/Qt/plugins/platforminputcontexts/libfcitxplatforminputcontextplugin.so
+
+
